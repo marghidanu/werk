f84d721c49563636fc85780a4f964e435632e8b4
Tudor Marghidanu <tudor@bit.fish>
2020-09-17 22:19:56 +0200

    Additional configuration tests

--- spec/config_spec.cr
+++ spec/config_spec.cr
@@ -2,9 +2,36 @@ require "./spec_helper"
 
 describe "Config" do
   it "should load the config file" do
-    content = File.read("werk.yml")
-    config = Werk::Model::Config.from_yaml(content)
+    config = Werk::Model::Config.load_file("werk.yml")
 
     config.is_a?(Werk::Model::Config).should eq true
+  end
+
+  it "should fail for non existing file" do
+    expect_raises(Exception, "Configuration file missing!") do
+      Werk::Model::Config.load_file("werk.yaml")
+    end
+  end
+
+  it "should fail for empty content" do
+    temp = File.tempfile
+    File.write(temp.path, "")
+
+    expect_raises(Exception, "Configuration file is empty!") do
+      Werk::Model::Config.load_file(temp.path)
+    end
+
+    temp.delete
+  end
+
+  it "should fail with parsing error" do
+    temp = File.tempfile
+    File.write(temp.path, "jobs: []")
+
+    expect_raises(Exception, "Parse error: #{temp.path}:1:7") do
+      Werk::Model::Config.load_file(temp.path)
+    end
+
+    temp.delete
   end
 end
