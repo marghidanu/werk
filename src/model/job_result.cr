8c11c730f2924165c69717926bdef39b1c82245c
Tudor Marghidanu <tudor@bit.fish>
2020-09-23 15:53:20 +0200

    Additional models documentation

--- src/model/job_result.cr
+++ src/model/job_result.cr
@@ -4,18 +4,25 @@ module Werk::Model
   class JobResult
     include JSON::Serializable
 
+    # Job name
     property name : String
 
+    # All variables passed to the job
     property variables : Hash(String, String)
 
+    # Job script content
     property content : String
 
+    # The directory in which the job was executed
     property directory : String
 
+    # Execution exit code
     property exit_code : Int32
 
+    # Job output (stdout & sterr combined)
     property output : String
 
+    # Duration of the job execution
     property duration : Float64
 
     def initialize(@name, @variables, @content, @directory, @exit_code, @output, @duration)
--- src/model/report.cr
+++ src/model/report.cr
@@ -4,12 +4,16 @@ module Werk::Model
   class Report
     include JSON::Serializable
 
+    # Unix epoch when this report was created
     property created : Int64
 
+    # Target job name
     property target : String
 
+    # Execution plan
     property plan : Array(Array(String))
 
+    # Jobs results
     property jobs = Hash(String, Werk::Model::JobResult).new
 
     def initialize(@target, @plan)
