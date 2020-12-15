45f4b40b80822bfd5a99b04b7f37f10c54dd17eb
Tudor Marghidanu <tudor@bit.fish>
2020-12-15 10:00:38 +0200

    Code cleanup

--- src/command/run.cr
+++ src/command/run.cr
@@ -40,35 +40,37 @@ module Werk::Command
       scheduler = Werk::Scheduler.new(config)
       report = scheduler.run(target, flags.context)
 
-      if flags.report
-        table = Tallboy.table do
-          header do
-            cell "Name", align: :center
-            cell "Stage", align: :center
-            cell "Status", align: :center
-            cell "Exit code", align: :center
-            cell "Duration", align: :center
-            cell "Executor", align: :center
-          end
+      display_report(report) if flags.report
+    end
 
-          report.plan.each_with_index do |stage, index|
-            stage.each do |name|
-              job = report.jobs[name]
+    def display_report(report)
+      table = Tallboy.table do
+        header do
+          cell "Name", align: :center
+          cell "Stage", align: :center
+          cell "Status", align: :center
+          cell "Exit code", align: :center
+          cell "Duration", align: :center
+          cell "Executor", align: :center
+        end
 
-              row border: :bottom do
-                cell job.name
-                cell index
-                cell (job.exit_code == 0) ? "OK".colorize(:green) : "Failed".colorize(:red), align: :center
-                cell job.exit_code
-                cell sprintf("%.3f secs", job.duration)
-                cell "Shell"
-              end
+        report.plan.each_with_index do |stage, index|
+          stage.each do |name|
+            job = report.jobs[name]
+
+            row border: :bottom do
+              cell job.name
+              cell index
+              cell (job.exit_code == 0) ? "OK".colorize(:green) : "Failed".colorize(:red), align: :center
+              cell job.exit_code
+              cell sprintf("%.3f secs", job.duration)
+              cell "Shell"
             end
           end
         end
-
-        puts table
       end
+
+      puts table
     end
   end
 end
--- src/utils/colors.cr
+++ src/utils/colors.cr
@@ -2,7 +2,22 @@ require "colorize"
 
 module Werk::Utils
   class Colors
-    getter colors = [91, 92, 93, 94, 95, 96, 31, 32, 33, 34, 35, 36, 37, 90]
+    getter colors = [
+      Colorize::ColorANSI::LightRed,
+      Colorize::ColorANSI::LightGreen,
+      Colorize::ColorANSI::LightYellow,
+      Colorize::ColorANSI::LightBlue,
+      Colorize::ColorANSI::LightMagenta,
+      Colorize::ColorANSI::LightCyan,
+      Colorize::ColorANSI::Red,
+      Colorize::ColorANSI::Green,
+      Colorize::ColorANSI::Yellow,
+      Colorize::ColorANSI::Blue,
+      Colorize::ColorANSI::Magenta,
+      Colorize::ColorANSI::Cyan,
+      Colorize::ColorANSI::LightGray,
+      Colorize::ColorANSI::DarkGray,
+    ]
 
     def initialize
       @index = 0
@@ -10,7 +25,7 @@ module Werk::Utils
 
     def next_color : Colorize::Color
       @index = 0 if @index >= @colors.size
-      color = Colorize::ColorANSI.new(@colors[@index])
+      color = @colors[@index]
       @index += 1
 
       color
