51dae76f789544679556ec8399e01b696aa13d21
Tudor <tudor@marghidanu.com>
2021-01-12 14:49:07 +0200

    Cleanup

--- src/werk.cr
+++ src/werk.cr
@@ -8,9 +8,6 @@ module Werk
     define_version Werk::VERSION
     define_help description: "Werk"
 
-    register_sub_command init : Werk::Command::Init,
-      description: "Create an empty configuration file"
-
     register_sub_command plan : Werk::Command::Plan,
       description: "Display execution plan"
 
