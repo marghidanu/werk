43236348161cd6d5ba180c0501a736466c090ffa
Tudor <tudor@marghidanu.com>
2021-12-26 21:05:23 +0200

    Moved application to separate file

--- src/werk.cr
+++ src/werk.cr
@@ -1,25 +1,6 @@
-require "admiral"
 require "log"
 
-require "./version"
-require "./commands/*"
-
-module Werk
-  class Application < Admiral::Command
-    define_version Werk::VERSION
-    define_help description: "Werk"
-
-    register_sub_command plan : Werk::Command::Plan,
-      description: "Display execution plan"
-
-    register_sub_command run : Werk::Command::Run,
-      description: "Run a job by name"
-
-    def run
-      puts help
-    end
-  end
-end
+require "./application"
 
 begin
   Log.setup_from_env(
