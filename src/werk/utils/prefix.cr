b2646c6443b495ab995d7572ca0ac3a66658e6b7
Tudor Mărghidanu <tudor@bit.fish>
2026-02-24 17:54:52 +0200

    Fixed PrefixIO by using atomic line writes

--- src/werk/utils/prefix.cr
+++ src/werk/utils/prefix.cr
@@ -2,11 +2,14 @@ module Werk::Utils
   class PrefixIO < IO
     ANSI_RESET = "\033[0m"
 
+    # Strip cursor movement/positioning, but keep color (SGR) codes
+    ANSI_STRIP = /\x1B\[[0-9;]*[A-HJ-K]/
+
     class_property? enabled : Bool = true
     @@mutex = Mutex.new
 
     @color : Colorize::Color
-    @new_line : Bool = true
+    @buffer : String = ""
 
     def initialize(@output : IO, @prefix : String)
       @color = Werk::Utils::Colors.next_color
@@ -22,19 +25,31 @@ module Werk::Utils
       return unless self.class.enabled?
       return if slice.empty?
 
-      @@mutex.synchronize do
-        data = String.new(slice)
-          .gsub(/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]/, "")
-          .gsub("\r", "\n")
+      data = String.new(slice)
+        .gsub(ANSI_STRIP, "")
+        .gsub("\r", "\n")
 
-        data.each_char do |char|
-          @output.print("[#{@prefix.colorize(@color)}] ") if @new_line
-          @output.print(char)
-          @new_line = (char == '\n')
+      data.each_line(chomp: false) do |line|
+        @buffer += line
+        if @buffer.ends_with?('\n')
+          flush_line
         end
       end
     rescue ex : IO::Error
       nil
+    end
+
+    # Flush any remaining partial line (e.g. when the process exits)
+    def close : Nil
+      flush_line unless @buffer.empty?
+      super
+    end
+
+    private def flush_line
+      @@mutex.synchronize do
+        @output.print("#{ANSI_RESET}[#{@prefix.colorize(@color)}] #{@buffer}")
+      end
+      @buffer = ""
     end
   end
 end
